e233accf56615a1a84ebcc888a2fbcf8691876df
Zulya Dzulkefli <zelia442@gmail.com>
2016-06-28 14:15:30 +1200

    made adjustments to nav

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 $(window).on('scroll',function() {
 
-        $('#mainNav').css('background','#ffffff');
+        $('#mainNav').css('background','rgba(255,255,255,0.7)');
+        $('.navbar-default .navbar-nav>li>a').css('color', '#4D4D4D');
 
 
       });
